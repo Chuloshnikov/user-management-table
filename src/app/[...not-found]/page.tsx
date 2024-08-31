8a77d6fe825a2cb383e1c801297380b71a610ea9
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-31 08:00:45 +0300

    done

---
+++ src/app/[...not-found]/page.tsx
@@ -0,0 +1,17 @@
+export default function NotFount() {
+    return (
+        <section className="ml-[10%] min-h-screen n py-12 px-8 basis-6/10">
+            <div className="flex-col items-center justify-center">
+                <div className="flex flex-col gap-6">
+                    <div>
+                        <h2 className='text-4xl font-bold text-[#5FA7EE]'>SEPOLTURE</h2>
+                        <p className='text-base -mt-2.5'>Admin panel</p>
+                    </div>
+                    <div className="text-center">
+                        <p className="text-2xl">Page not found</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
