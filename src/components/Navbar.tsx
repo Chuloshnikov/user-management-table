2302465de3be2d208cfac6f7fb11ffa93d84d476
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-09-02 10:14:10 +0300

    link active mark added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
+ const pathname = usePathname();
+
   return (
     <div className='basis-[10%] shadow-2xl fixed top-0 left-0 h-screen w-[10%]'>
         <div>
@@ -11,10 +15,10 @@ const Navbar = () => {
           </div>
           <nav className='px-5 py-10'>
               <ul className='flex flex-col gap-4'>
-                  <li><Link className={`hover:text-[#5FA7EE] duration-300`} href={"/"}>users</Link></li>
-                  <li><Link className="hover:text-[#5FA7EE] duration-300" href={"/dashboard"}>dashboard</Link></li>
-                  <li><Link className="hover:text-[#5FA7EE] duration-300" href={"/orders"}>orders</Link></li>
-                  <li><Link className="hover:text-[#5FA7EE] duration-300" href={"/settings"}>settings</Link></li>
+                  <li><Link className={`hover:text-[#5FA7EE] duration-300 ${pathname === "/" && "underline decoration-4"}`} href={"/"}>users</Link></li>
+                  <li><Link className={`hover:text-[#5FA7EE] duration-300 ${pathname === "/dashboard" && "underline decoration-4"}`} href={"/dashboard"}>dashboard</Link></li>
+                  <li><Link className={`hover:text-[#5FA7EE] duration-300 ${pathname === "/orders" && "underline decoration-4"}`} href={"/orders"}>orders</Link></li>
+                  <li><Link className={`hover:text-[#5FA7EE] duration-300 ${pathname === "/settings" && "underline decoration-4"}`} href={"/settings"}>settings</Link></li>
               </ul>
             </nav>
         </div>
